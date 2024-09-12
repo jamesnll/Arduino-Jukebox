dc6f439e68bfb4412d9d830f3465dd1dd83fa215
jamesnll <langillejames11@gmail.com>
2024-09-11 19:35:55 -0700

    Moved switch case hex codes to defined constants in the header file

--- ir_receiver.h
+++ ir_receiver.h
@@ -4,6 +4,29 @@
 #include <Arduino.h>
 #include <IRremote.h>
 
+// IR remote button mappings
+#define POWER         0xBA45FF00
+#define FUNC_STOP     0xB847FF00
+#define VOL_UP        0xB946FF00
+#define FAST_BACK     0xBB44FF00
+#define PAUSE_RESUME  0xBF40FF00
+#define FAST_FORWARD  0xBC43FF00
+#define DOWN          0xF807FF00
+#define VOL_DOWN      0xEA15FF00
+#define UP            0xF609FF00
+#define EQ            0xE619FF00
+#define ST_REPT       0xF20DFF00
+#define ZERO          0xE916FF00
+#define ONE           0xF30CFF00
+#define TWO           0xE718FF00
+#define THREE         0xA15EFF00
+#define FOUR          0xF708FF00
+#define FIVE          0xE31CFF00
+#define SIX           0xA55AFF00
+#define SEVEN         0xBD42FF00
+#define EIGHT         0xAD52FF00
+#define NINE          0xB54AFF00
+
 static const int IR_RECEIVER_PIN = 11; // Signal pin of the IR receiver
 
 class IR_Receiver {
--- ir_receiver.ino
+++ ir_receiver.ino
@@ -28,27 +28,27 @@ void IR_Receiver::translate_ir()
     // Mapping IR codes to button names
     switch (irrecv->decodedIRData.decodedRawData) 
     {
-      case 0xBA45FF00: Serial.println("POWER"); break;
-      case 0xB847FF00: Serial.println("FUNC/STOP"); break;
-      case 0xB946FF00: Serial.println("VOL+"); break;
-      case 0xBB44FF00: Serial.println("FAST BACK"); break;
-      case 0xBF40FF00: Serial.println("PAUSE"); break;
-      case 0xBC43FF00: Serial.println("FAST FORWARD"); break;
-      case 0xF807FF00: Serial.println("DOWN"); break;
-      case 0xEA15FF00: Serial.println("VOL-"); break;
-      case 0xF609FF00: Serial.println("UP"); break;
-      case 0xE619FF00: Serial.println("EQ"); break;
-      case 0xF20DFF00: Serial.println("ST/REPT"); break;
-      case 0xE916FF00: Serial.println("0"); break;
-      case 0xF30CFF00: Serial.println("1"); break;
-      case 0xE718FF00: Serial.println("2"); break;
-      case 0xA15EFF00: Serial.println("3"); break;
-      case 0xF708FF00: Serial.println("4"); break;
-      case 0xE31CFF00: Serial.println("5"); break;
-      case 0xA55AFF00: Serial.println("6"); break;
-      case 0xBD42FF00: Serial.println("7"); break;
-      case 0xAD52FF00: Serial.println("8"); break;
-      case 0xB54AFF00: Serial.println("9"); break;
+      case POWER: Serial.println("POWER"); break;
+      case FUNC_STOP: Serial.println("FUNC/STOP"); break;
+      case VOL_UP: Serial.println("VOL+"); break;
+      case FAST_BACK: Serial.println("FAST BACK"); break;
+      case PAUSE_RESUME: Serial.println("PAUSE"); break;
+      case FAST_FORWARD: Serial.println("FAST FORWARD"); break;
+      case DOWN: Serial.println("DOWN"); break;
+      case VOL_DOWN: Serial.println("VOL-"); break;
+      case UP: Serial.println("UP"); break;
+      case EQ: Serial.println("EQ"); break;
+      case ST_REPT: Serial.println("ST/REPT"); break;
+      case ZERO: Serial.println("0"); break;
+      case ONE: Serial.println("1"); break;
+      case TWO: Serial.println("2"); break;
+      case THREE: Serial.println("3"); break;
+      case FOUR: Serial.println("4"); break;
+      case FIVE: Serial.println("5"); break;
+      case SIX: Serial.println("6"); break;
+      case SEVEN: Serial.println("7"); break;
+      case EIGHT: Serial.println("8"); break;
+      case NINE: Serial.println("9"); break;
       default:
         Serial.println("Other button");
         break;
