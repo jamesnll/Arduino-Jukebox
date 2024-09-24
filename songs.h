520b6ebda2004ecfdd95247cfcfd3bb9693398ea
jamesnll <langillejames11@gmail.com>
2024-09-23 19:06:05 -0700

    Added songs file to recreate and play songs. Created song 1 chorus(Better Off Aloen by Alice Deejay)

---
+++ songs.h
@@ -0,0 +1,15 @@
+#ifndef SONGS_H
+#define SONGS_H
+
+#include "pitches.h"
+
+#define HALF_BEAT 2000
+#define QUARTER_BEAT 1000
+#define EIGHTH_BEAT 500
+#define SIXTEENTH_BEAT 250
+
+#define NO_NOTE 0
+
+void play_song_one();
+
+#endif
---
+++ songs.ino
@@ -0,0 +1,49 @@
+#include "songs.h"
+
+void play_song_one()
+{
+  int eighth_beat_tempo = EIGHTH_BEAT / 2.273;
+  int sixteenth_beat_tempo = SIXTEENTH_BEAT / 2.273;
+  const int NUM_NOTES = 10;
+
+  int melody[NUM_NOTES] = {
+    NOTE_B4, NOTE_B4, NOTE_GS4, NOTE_B4,
+    NOTE_B4, NOTE_AS4, NOTE_FS4, NOTE_FS5,
+    NOTE_FS5, NOTE_DS5
+  };
+
+  int rests[NUM_NOTES] = {
+    eighth_beat_tempo * 2,
+    eighth_beat_tempo + 50,
+    eighth_beat_tempo * 2,
+    eighth_beat_tempo * 2,
+    eighth_beat_tempo * 2,
+    eighth_beat_tempo * 2,
+    eighth_beat_tempo + 50,
+    eighth_beat_tempo + sixteenth_beat_tempo,
+    eighth_beat_tempo + sixteenth_beat_tempo,
+    eighth_beat_tempo
+  };
+
+  // Play the song chorus
+  for (int i = 0; i < 4; i++)
+  {
+
+    // Different high note on the 2nd & 4th time
+    if (i % 2 != 0)
+    {
+      melody[7] = NOTE_E5;
+      melody[8] = NOTE_E5;
+    }
+    else
+    {
+      melody[7] = NOTE_FS5;
+      melody[8] = NOTE_FS5;
+    }
+    for (int curr_note = 0; curr_note < NUM_NOTES; curr_note++)
+    {
+      tone(8, melody[curr_note], eighth_beat_tempo);
+      delay(rests[curr_note]);
+    }
+  }
+}
