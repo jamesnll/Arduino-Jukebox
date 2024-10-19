c6fe775a66a7eecaa471708d33af8189d6190b6d
jamesnll <langillejames11@gmail.com>
2024-10-18 17:02:39 -0700

    Added required setup code for the DFPlayer Mini module

--- Arduino-Jukebox.ino
+++ Arduino-Jukebox.ino
@@ -1,16 +1,47 @@
 #include "ir_receiver.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
+// Create an object for the IR receiver
 IR_Receiver& ir_receiver = IR_Receiver::get_instance();
+
+// Initialize software serial on pins 5 and 6
+SoftwareSerial mySoftwareSerial(5, 6);  // RX, TX
+// Declare an object for the DFPlayer Mini
+DFRobotDFPlayerMini myDFPlayer;
 
 /**
  * Setup the IR receiver, Serial Monitor to the Arduino board
  */
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("IR Receiver Button Decode");
+  // Serial communication with the module
+  mySoftwareSerial.begin(9600);
+  // Initialize Arduino serial
+  Serial.begin(115200);
+  // Check if the module is responding and if the SD card is found
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini"));
+  Serial.println(F("Initializing DFPlayer module ... Wait!"));
+
+  // Check for DFPlayer Mini initialization errors
+  if (!myDFPlayer.begin(mySoftwareSerial)) {
+    Serial.println(F("Not initialized:"));
+    Serial.println(F("1. Check the DFPlayer Mini connections"));
+    Serial.println(F("2. Insert an SD card"));
+    while (true)
+      ;
+  }
+
+  Serial.println();
+  Serial.println(F("DFPlayer Mini module initialized!"));
+
+  delay(1000); // Short delay before starting the IR receiver
+
   // Start the receiver
   ir_receiver.begin();
+  Serial.println();
+  Serial.println(F("IR receiver initialized!"));
 }
 
 /**
